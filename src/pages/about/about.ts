d665804da00e04c00a2b399dfaeb83ea313e21d2
Osmar Coronel <OsmarCoronel@wirelessprv-10-192-234-234.near.illinois.edu>
2017-02-24 21:22:03 -0600

    added camera function

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -2,14 +2,31 @@ import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
 
+import { Camera } from 'ionic-native';
+
+
+
+
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
 export class AboutPage {
 
-  constructor(public navCtrl: NavController) {
+  public base64Image: string;
+  constructor() {}
 
-  }
+    takePicture(){Camera.getPicture({
+    destinationType: Camera.DestinationType.DATA_URL,
+        targetWidth: 1000,
+        targetHeight: 1000
+    }).then((imageData) => {
+     // imageData is either a base64 encoded string or a file URI
+     // If it's base64:
+     let base64Image = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+     // Handle error
+    });
+    }
 
 }
